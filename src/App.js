f52227f44ae89f7d87363653f20c045234f47b6f
Juan <jd.berbeo@gmail.com>
2021-05-25 19:17:22 -0500

    2do commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+function App() {
+  const [value, setValue] = useState('')
+
+
+  const handleOnChange = (e) => {
+  setValue(e.target.value) 
+}
+console.log(value) 
+ 
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={handleOnChange}/>
+        <p className="repeater">{value}</p>
       </div>
     );
-  }
 }
 
 export default App;
